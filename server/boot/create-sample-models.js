1c687955fb6c3ba0010ef69efe9a63a595ef6ac3
William Wang <shadow25251@gmail.com>
2015-02-24 22:14:11 +0800

    firm

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,25 +1,18 @@
 module.exports = function(app) {
 	app.dataSources.vcdata.automigrate('member', function(err) {
 	    if (err) throw err;
-
-
-	    app.models.member.create([
-	      {member_name: 'William Wang', member_id: 'test_id_01'}
-	    ], function(err, members) {
-	      if (err) throw err;
-	      console.log('Models created: \n', members);
-	    });
-	  });
-
-	app.dataSources.vcdata.automigrate('firm-record', function(err) {
-	    if (err) throw err;
-
-
 	    // app.models.member.create([
 	    //   {member_name: 'William Wang', member_id: 'test_id_01'}
 	    // ], function(err, members) {
 	    //   if (err) throw err;
 	    //   console.log('Models created: \n', members);
 	    // });
+	  });
+
+	app.dataSources.vcdata.automigrate('firm-record', function(err) {
+	    if (err) throw err;
+	  });
+	app.dataSources.vcdata.automigrate('firm', function(err) {
+	    if (err) throw err;
 	  });
 };
